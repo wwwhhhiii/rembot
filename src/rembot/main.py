5cbd5ee518b6969a6cb5d57dd8e0bfaa4172dd33
Alexander <alexandr.s.trushnikov@gmail.com>
2024-02-10 20:15:53 +0500

    Added telegram bot handlers

--- src/rembot/main.py
+++ src/rembot/main.py
@@ -1,4 +1,5 @@
 import uvloop
+from loguru import logger
 
 from settings.bot import bot_settings
 from telegram.bot import create_bot, create_dispatcher
@@ -7,8 +8,13 @@ from telegram.handlers import router
 
 async def main() -> None:
     
+    logger.debug("Starting app")
+
     bot = create_bot(bot_settings.token)
     dispatcher = create_dispatcher(include_routers=[router,])
+
+    logger.info("Starting polling...")
+
     await dispatcher.start_polling(bot)
 
 
--- src/rembot/telegram/handlers.py
+++ src/rembot/telegram/handlers.py
@@ -1,12 +1,17 @@
 import aiogram
-from aiogram.filters import CommandStart
+from aiogram.filters import Command
+from aiogram.filters.command import CommandObject
 from loguru import logger
 
+from telegram.utils import (
+    validate_and_parse_remind_cmd_args,
+)
 
-router = aiogram.Router()
+
+router = aiogram.Router(name="rem")
 
 
-@router.message(CommandStart)
+@router.message(Command("start"))
 async def cmd_start_handler(
     message: aiogram.types.Message,
 ) -> None:
@@ -21,8 +26,10 @@ async def cmd_start_handler(
     await message.answer("Reminder bot is started")
 
 
+@router.message(Command("remind"))
 async def cmd_create_reminder_handler(
     message: aiogram.types.Message,
+    command: CommandObject,
 ) -> None:
     """
     Handles `/remind` command.
@@ -30,5 +37,12 @@ async def cmd_create_reminder_handler(
     ...
     """
 
-    ...
+    logger.debug(f"Recieved /remind command from {message.from_user.username}")
+    
+    args_valid, args = validate_and_parse_remind_cmd_args(command.args)
+    if not args_valid:
+        logger.debug(f"Invalid remind command args: {command.args}")
+        return
+
+    await message.answer(f"Recieved remind cmd {args}")
 
