97e5c8d02050cf94934d8dc110b99678097e2411
Alexander <alexandr.s.trushnikov@gmail.com>
2024-02-18 12:07:59 +0500

    Small task args serialization fix

--- src/rembot/tasks.py
+++ src/rembot/tasks.py
@@ -1,7 +1,9 @@
 import uuid
 import datetime
+from typing import Any
 
 import gevent
+from loguru import logger
 
 from celery_app import app
 from models import (
@@ -10,7 +12,9 @@ from models import (
 
 
 @app.task
-def exec_reminder(reminder: Reminder) -> ...:
+def exec_reminder(reminder: dict[str, Any]) -> ...:
+
+    reminder_ = Reminder(**reminder)
 
     def _finalize_reminder(reminder_id: uuid.UUID) -> None:
         ...
@@ -19,10 +23,13 @@ def exec_reminder(reminder: Reminder) -> ...:
         # TODO dispatch the reminder
         # TODO delete the reminder from db
 
+        logger.debug("Reminder is finalized")
+
     def _start_off_reminder(wait_until: datetime.datetime) -> None:
         wait_time_sec = (wait_until - datetime.datetime.now()).total_seconds()
+        logger.debug(f"Waiting reminder for {wait_time_sec} sec")
         gevent.sleep(seconds=wait_time_sec)
 
-    _start_off_reminder(wait_until=reminder.time)
-    _finalize_reminder(reminder_id=reminder.id)
+    _start_off_reminder(wait_until=reminder_.time)
+    _finalize_reminder(reminder_id=reminder_.id)
     
