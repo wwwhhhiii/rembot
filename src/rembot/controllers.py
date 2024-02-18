dd03756d6a40645d3137bf5e4c8d18bae3c7c0cb
Alexander <alexandr.s.trushnikov@gmail.com>
2024-02-18 12:09:12 +0500

    Initial controllers setup

--- src/rembot/controllers.py
+++ src/rembot/controllers.py
@@ -1,30 +1,59 @@
 import uuid
+import enum
 
-from src.rembot.tasks import exec_reminder
-from src.rembot.models import (
+from loguru import logger
+from attrs import asdict
+
+from tasks import exec_reminder
+from models import (
+    User,
     Reminder,
     ReminderCreateRequest,
     ReminderGetRequest,
     ReminderUpdateRequest,
     ReminderDeleteRequest,
 )
+from db import queries as db_queries
 
 
-def create_reminder(request: ReminderCreateRequest) -> ...:
+class RequestExecStatus(int, enum.Enum):
+    OK = enum.auto()
+    INVALID = enum.auto()
+    DB_ERROR = enum.auto()
+    TASK_ERROR = enum.auto()
+
+
+async def create_reminder(request: ReminderCreateRequest) -> RequestExecStatus:
     """"""
 
-    reminder = Reminder(
+    try:
+        reminder = Reminder(
+            id=uuid.uuid4(),
+            time=request.time,
+            text=request.text)
+    except Exception as e:
+        logger.error(f"Invalid reminder create request: {request}")
+        return RequestExecStatus.INVALID
+
+
+    user = User(
         id=uuid.uuid4(),
-        reply_to_id=request.user_tg_id,
-        username=request.username,
-        time=request.time,
-        text=request.text)
-    
-    # TODO add to db
+        tg_id=request.user_tg_id,
+        username=request.username)
+    await db_queries.create_user(user)
+    try:
+        await db_queries.create_reminder(reminder, user_id=user.id)
+    except Exception as e:
+        logger.error(f"Database error: {e}")
+        return RequestExecStatus.DB_ERROR
 
-    res = exec_reminder.delay(reminder)
+    try:
+        exec_reminder.delay(asdict(reminder))
+    except Exception as e:
+        logger.error(f"Task error: {e}")
+        return RequestExecStatus.TASK_ERROR
 
-    # TODO return status
+    return RequestExecStatus.OK
 
 
 def get_reminder(request: ReminderGetRequest) -> ...:
@@ -61,4 +90,8 @@ def delete_reminder(request: ReminderDeleteRequest) -> ...:
     """"""
 
     # TODO load reminder from db, check if exists
-    # TODO stop worker, delete reminder from db 
+    # TODO stop worker, delete reminder from db
+
+
+def dispatch_reminder_to_user(request: ...) -> ...:
+    ...
