4ba38d173a8e422bf81a9955c9d469bd16e186e1
Alexander <alexandr.s.trushnikov@gmail.com>
2024-03-03 17:28:09 +0500

    celery configurations

--- src/rembot/celery_app.py
+++ src/rembot/celery_app.py
@@ -1,9 +1,21 @@
 from celery import Celery
+import tzlocal
 
 from messaging_settings import messaging_settings
+
+
+POOL = "gevent"
+CONCURRENCY = 500
+
+RESULT_BACKEND = "rpc://"
+TZ_NAME = tzlocal.get_localzone_name()
+
+REMINDERS_CHECK_INTEVAL_SEC = 30 * 60
 
 
 app = Celery(
     'reminders',
     broker=messaging_settings.broker_connection_string,
-    backend="rpc://")
+    backend=RESULT_BACKEND,)
+
+app.conf.timezone = TZ_NAME
