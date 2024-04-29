fd2fe60af190e7d40ed1809a14a685c1ead7543e
Alexander <alexandr.s.trushnikov@gmail.com>
2024-04-29 20:00:08 +0500

    Updated test case

--- tests/test_database.py
+++ tests/test_database.py
@@ -9,16 +9,17 @@ from sqlalchemy.ext.asyncio import (
 
 from src.rembot.db import queries
 from src.rembot.db.settings import db_settings
-from src.rembot.db.models import User, Reminder
+from src.rembot.db.models import DBUser, DBReminder
 
 
 engine = create_async_engine(db_settings.connection_string)
 session_factory = async_sessionmaker(engine)
 
 
-@pytest.mark.asyncio
+@pytest.mark.asyncio  # type: ignore[misc]
 async def test_get_reminders_in_timeframe() -> None:
-    
+    """Test that ensures that reminders are collected in a specific time frame"""
+
     start = datetime.datetime(year=2000, month=2, day=16, hour=1)
     end = start + datetime.timedelta(hours=5)
 
@@ -31,29 +32,28 @@ async def test_get_reminders_in_timeframe() -> None:
         async with session.begin():
             session.add_all(
                 [
-                    User(
+                    DBUser(
                         tg_id=00000,
                         username="000",
                         reminders=[
-                            Reminder(time=out1, text="I'm out"),
-                            Reminder(time=in1, text="I'm in"),
+                            DBReminder(time=out1, text="I'm out"),
+                            DBReminder(time=in1, text="I'm in"),
                         ],
                     ),
-                    User(
+                    DBUser(
                         tg_id=11111,
                         username="111",
                         reminders=[
-                            Reminder(time=in2, text="I'm in"),
-                            Reminder(time=out2, text="I'm out")
+                            DBReminder(time=in2, text="I'm in"),
+                            DBReminder(time=out2, text="I'm out"),
                         ],
                     ),
                 ]
             )
 
     async with session_factory() as session:
-        res = await queries.get_reminders_within_time(
-            session, start, end)
-    
+        res = await queries.get_reminders_within_time(session, start, end)
+
     in_time = set((in1, in2))
     got_time = set([rem.time for rem in res])
 
