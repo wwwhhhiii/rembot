ab9be302486696a80b70e8dbc4fcd06f50546944
Alexander <alexandr.s.trushnikov@gmail.com>
2024-06-28 21:35:10 +0500

    Removed unused multiprocessing module

--- src/rembot/main.py
+++ src/rembot/main.py
@@ -1,5 +1,3 @@
-import multiprocessing
-
 import uvloop
 from loguru import logger
 
@@ -11,11 +9,15 @@ from db.settings import db_settings
 
 async def main() -> None:
     """Application entrypoint"""
-    
+
     logger.info("Starting app...")
 
     bot = create_bot(bot_settings.token)
-    dispatcher = create_dispatcher(include_routers=[router,])
+    dispatcher = create_dispatcher(
+        include_routers=[
+            router,
+        ]
+    )
 
     logger.info("App started")
     await dispatcher.start_polling(bot)
